991c53fd886025fa879e383b07e6b02300613e8c
Fattah Maulana <54757764+fattahmaulana@users.noreply.github.com>
2025-05-30 21:12:30 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { Approach } from "@/components/approach";
-import { Clients } from "@/components/clients";
-import { Experience } from "@/components/experience";
 import { Footer } from "@/components/footer";
 import { Grid } from "@/components/grid";
-import { Hero } from "@/components/hero";
 import { FloatingNav } from "@/components/ui/floating-nav";
 import { RecentProjects } from "@/components/recent-projects";
 import { navItems } from "@/data";
